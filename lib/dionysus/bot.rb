2a0d1e69d476a866d6faaa98aa2e2a4553fd1de5
Jelizondo <jelizondolevy.s@gmail.com>
2017-05-29 16:12:37 -0500

    Fixes document_conversion method

--- lib/dionysus/bot.rb
+++ lib/dionysus/bot.rb
@@ -1,10 +1,9 @@
 require "olimpo"
 require "dionysus/bot/version"
-require "dionysus/bot/document_conversion"
 
 module Dionysus
   module Bot
     extend Olimpo
-      autoload :DocumentConversation, "dionysus/bot/documentconversion"
+      autoload :DocumentConversion, "dionysus/bot/document_conversion"
   end
 end
--- lib/dionysus/bot/document_conversion.rb
+++ lib/dionysus/bot/document_conversion.rb
@@ -1,18 +1,13 @@
 module Dionysus
   module Bot
     class DocumentConversion < Olimpo::Base
-
       def self.convert_document(params = {})
         input_file = File.new(params[:document])
-
-        response = post("v1/convert_document?version=2015-12-15",
-                        document: input_file, config: params[:config])
-
+        response = post("/convert_document?version=2015-12-15", body: { file: input_file, config: { conversion_target: "ANSWER_UNITS" }.to_json })
+        
         return response if response.success?
-
         raise_exception(response.code, response.body)
       end
-
     end
   end
 end
